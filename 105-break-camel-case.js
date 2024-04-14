68cfe0041fbb6327ecb4b85a453fce83519e3901
Benedict Kenjiro <kenjiro442@gmail.com>
2024-04-14 12:12:51 +0800

    Day 105/366 - Daily Codewars Challenge

---
+++ 105-break-camel-case.js
@@ -0,0 +1,26 @@
+function solution(string) {
+    // if (string === '') return string;
+    // let array = string.split('');
+    // for (let i = 1; i < array.length; i++) {
+    //     if (array[i] === array[i].toUpperCase()) {
+    //         array.splice(i,0,' ');
+    //         i++;
+    //     }
+    // }
+    // return array.join('');
+
+    return string === '' ? '' : 
+        string.split('').map((e,i) => {
+            if (e === e.toUpperCase()) {
+                e = ` ${e}`;
+                i++;
+            }
+            return e
+        }).join('');
+
+    // return(string.replace(/([A-Z])/g, ' $1'));
+}
+
+console.log(solution('camelCasing'))
+console.log(solution('identifier'))
+console.log(solution(''))
