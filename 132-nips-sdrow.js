5340bb41d09cd87c48430e165e23ecacc23a33eb
Benedict Kenjiro <kenjiro442@gmail.com>
2024-05-11 02:06:44 +0800

    Day 132/366 - Daily Codewars Challenge

---
+++ 132-nips-sdrow.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5264d2b162488dc400000001/
+
+function spinWords(string){
+    return string.split(' ').map(e => e.length > 4 ? e.split('').reverse().join('') : e).join(' ');
+}
+
+console.log(spinWords("Hey fellow warriors"))
+console.log(spinWords("This is a test"))
+console.log(spinWords("This is another test"))
