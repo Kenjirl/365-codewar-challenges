79126ceb2cd4c21634f0890dd8f82ce805c2ad46
Benedict Kenjiro <kenjiro442@gmail.com>
2024-08-24 21:40:51 +0800

    # 151

--- 150-count-lowercase-letter.js
+++ 150-count-lowercase-letter.js
@@ -3,7 +3,18 @@
 
 function lowercaseCount(str){
     return str.replace(/([^a-z])+/g, '').length;
+
+    // return (str.match(/[a-z]/g) || []).length;
 }
 
 console.log(lowercaseCount('abc'));
 console.log(lowercaseCount('abcABC123'));
+
+// function lowercaseCount(str){
+    // str = str.replace(/([^a-z])+/g, '');
+    // let res = 0;
+    // for (let i = 0; i < str.length; i++) {
+    //     res+= str[i] === str[i].toLowerCase() ? 1 : 0;
+    // }
+    // return res;
+// }
---
+++ 151-array-filter.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/514a6336889283a3d2000001/
+// 28 Agustus 2024
+function getEvenNumbers(numbersArray){
+    return numbersArray.filter(e=>e%2==0);
+}
+
+console.log(getEvenNumbers([2,4,5,6]));
