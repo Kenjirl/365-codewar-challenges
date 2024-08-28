27b183ed0218a6f5f42bb7fdd137b3fa3f7b8a5c
Benedict Kenjiro <kenjiro442@gmail.com>
2024-08-28 13:40:56 +0800

    #155

---
+++ 155-twice-as-old.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5b853229cfde412a470000d0/
+// 28 Agustus 2024
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    return Math.abs(dadYearsOld - 2 * sonYearsOld);
+}
+
+console.log(twiceAsOld(36,7));
+console.log(twiceAsOld(55,30));
