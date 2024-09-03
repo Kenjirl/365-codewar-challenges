0c44b4871fa9b68b57949b5f610f988973b9e3a6
Benedict Kenjiro <kenjiro442@gmail.com>
2024-09-03 23:55:47 +0800

    #158

---
+++ 158-greet-me.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/535474308bb336c9980006f2
+// 3 September 2024
+
+var greet = function(name) {
+    return "Hello " + name.charAt(0).toUpperCase() + name.slice(1).toLowerCase() + "!";
+};
